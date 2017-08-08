4be914e0dbdf2abc8ca7f61d24283fd367ef7b91
Steve <https://bitbucket.org/eocampo27/seleniumproject.git>
2017-08-07 21:10:02 -0300

    UPDATE_LOGGED_PROPERLY_TC

--- test/specs/Login/LoggedProperly.js
+++ test/specs/Login/LoggedProperly.js
@@ -1,10 +1,15 @@
 //Created by steve on 03/08/17.
 assert = require('assert');
 LoginPage = require('../../../pageObjects/Login/Login.page');
+DashboardPage = require('../../../pageObjects/Login/Dashboard.page');
 
 describe('Login page\n', function () {
     it('User is logged properly - Right user and password', function () {
         LoginPage.open
-
+        LoginPage.inputEmail.setValue('admin@phptravels.com');
+        LoginPage.inputPassword.setValue('demoadmin');
+        LoginPage.clickButtonLogin;
+        var titlePage = DashboardPage.getPageTitle;
+        assert.strictEqual(titlePage,'Dashboard',"Failed: User Or Password isn't correct");
     });
 });
